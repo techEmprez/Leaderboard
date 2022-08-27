9f2f9335ae41865247712cae88557cf2dc6da71d
techEmprez <choiceosobor@gmail.com>
2022-08-27 20:58:50 +0100

    add webpack and index file

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,26 @@
-/* eslint-disable import/no-unresolved */
-const path = require('path');
+/* eslint-disable no-undef */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const { resolve } = require('path');
+const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
+  },
   output: {
-    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
   },
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
-      template: path.resolve(__dirname, './src/index.html'),
+      template: './src/index.html',
     }),
+
   ],
   module: {
     rules: [
@@ -25,9 +28,14 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
 };
