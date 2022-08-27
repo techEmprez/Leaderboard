10db1509ab494b5af1bd888dd177f0cab65db559
techEmprez <choiceosobor@gmail.com>
2022-08-27 19:48:32 +0100

    Add webpack file

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-unresolved */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { resolve } = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
